9afc1237bca1f84afd769705d4e8e3c4745b445f
Mili Mishra <mili.mishra@office365.3pillarglobal.com>
2022-05-24 14:15:58 +0530

    added meals summary and available meals list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import './App.css';
-
 import { Fragment } from 'react';
+
 import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </>
   );
 }
