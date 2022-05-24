d080b63f79ae155f8dab39fde5eb7fdb69dab39d
Mili Mishra <mili.mishra@office365.3pillarglobal.com>
2022-05-24 22:17:01 +0530

    added cart total on cart icon

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Fragment, useState } from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider'
 
 function App() {
   const [CartIsShown, setCartIsShown] = useState(false);
@@ -14,13 +15,13 @@ function App() {
   };
 
   return (
-    <>
+    <CartProvider>
       {CartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </>
+    </CartProvider>
   );
 }
 
